248f388af70056dcfa016ae12283b00b411217e3
Andrew Saputra <andrew.saputra@gmail.com>
2023-08-02 18:06:31 +0700

    add general ci-cd resources

---
+++ global/ci-cd/codepipeline.tf
@@ -0,0 +1,119 @@
+### IAM ROLE ###
+
+resource "random_id" "codepipeline_role" {
+  byte_length = 8
+
+  keepers = {
+    Target = "iam-codepipeline-general"
+  }
+
+  prefix = "role-codepipeline-general-"
+}
+
+resource "aws_iam_role" "codepipeline" {
+  name               = random_id.codepipeline_role.hex
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role.json
+
+  inline_policy {
+    name   = "permissions"
+    policy = data.aws_iam_policy_document.codepipeline_policy.json
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codedeploy:CreateDeployment",
+      "codedeploy:GetApplicationRevision",
+      "codedeploy:GetDeployment",
+      "codedeploy:GetDeploymentConfig",
+      "codedeploy:RegisterApplicationRevision",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codestar-connections:GetConnection",
+      "codestar-connections:UseConnection",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.codepipeline.arn}/*",
+    ]
+  }
+}
+
+
+### S3 ARTIFACTS BUCKET ###
+
+resource "random_id" "codepipeline_artifacts" {
+  byte_length = 8
+
+  keepers = {
+    Target = "codepipeline-artifacts"
+  }
+
+  prefix = "codepipeline-artifacts-"
+}
+
+resource "aws_s3_bucket" "codepipeline" {
+  bucket = random_id.codepipeline_artifacts.hex
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "codepipeline" {
+  bucket = aws_s3_bucket.codepipeline.id
+
+  rule {
+    id     = "expiration-rule"
+    status = "Enabled"
+
+    filter {
+    }
+
+    expiration {
+      days = 3
+    }
+  }
+}
